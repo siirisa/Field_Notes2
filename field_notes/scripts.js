cfb5692becbc6ec472d565d7a1b8badc762962b5
Siiri <36868246+siirisa@users.noreply.github.com>
2018-04-16 08:38:57 +0100

    changing field list structure
    added embedded google maps and changed the structure of how we present the info about the fields

---
+++ field_notes/scripts.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+sr.reveal('.hideme');
+
+
+
+
+
+
+
+<!--    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $(".hideme").each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},500);
+
+            }
+
+        });
+
+    });
+
+}); -->
