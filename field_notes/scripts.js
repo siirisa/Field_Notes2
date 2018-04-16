5fd68acc4cf49a454a1f8c0bd90bad1865ec3b43
Siiri <36868246+siirisa@users.noreply.github.com>
2018-04-16 17:39:28 +0100

    deleting irrelevant lines of code, organising
    deleted code from something I dont' want on the website anymore (text fading in). also started putting files into folders

--- field_notes/scripts.js
+++ field_notes/scripts.js
@@ -1,31 +1,3 @@
-/* this is the fade in effect in the home pictures */
-$(function(){  // $(document).ready shorthand
-  $('.fieldphoto').fadeIn('slow');
-});
-
-$(document).ready(function() {
-
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-
-           var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},1500);
-
-            }
-
-        });
-
-    });
-
-});
-
 /* scroll up button */
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
