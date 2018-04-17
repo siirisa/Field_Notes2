68560fa2f0dd191b44f626dd1b831ef8e32388a1
Carolina <cn400@bath.ac.uk>
2018-04-17 15:41:14 +0100

    deleted extra js
    this is extra js we had added to try for nav bar but now not relevant

--- field_notes/scripts.js
+++ field_notes/scripts.js
@@ -15,9 +15,3 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
-
-$('[data-toggle=collapse]').hover(function (e) {
-  $(this).click();
-});
